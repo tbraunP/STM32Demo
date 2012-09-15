d57f179ca5db0c001fef66de7cef825969691d58
Tobias <tbraun@inf>
2012-09-15 19:57:12 +0200

    G++

---
+++ makeC++.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+repl=("Debug/makefile" "Debug/src/subdir.mk" "Debug/STMPeriLib/subdir.mk")
+
+for f in ${repl[@]}; do
+	echo "Replacing arm-none-eabi-gcc with arm-none-eabi-g++ in ${f}"
+	sed -i 's/arm-none-eabi-gcc/arm-none-eabi-g++/g' $f
+done
+
+echo "Building"
+cd Debug
+make clean
+make all -j
+cd ..
+
+echo "Uploading..."
+
